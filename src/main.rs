5094de374e34e180cade0851d1cbd550ded946d4
lomirus <lomirus@qq.com>
2022-05-08 00:34:04 +0800

    feat(ui): switch stack to main panel

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
-use relm4::{adw, gtk, AppUpdate, Model, RelmApp, Widgets};
+use relm4::{adw, gtk, send, AppUpdate, Model, RelmApp, Widgets};
 
 use adw::{ApplicationWindow, HeaderBar};
-use gtk::{Align, Box, Entry, Grid, Label, Orientation, Button};
+use gtk::{Align, Box, Button, Entry, Grid, Label, Orientation, Stack, StackTransitionType};
 
 use adw::prelude::*;
 
-#[derive(Default)]
-struct AppModel;
+struct AppModel {
+    page: Page,
+}
 
-enum Message {}
+enum Page {
+    Login,
+    Main,
+}
+
+enum Message {
+    LoginSuccessful,
+}
 
 impl AppUpdate for AppModel {
     fn update(
         &mut self,
-        _msg: Self::Msg,
+        msg: Self::Msg,
         _components: &Self::Components,
         _sender: relm4::Sender<Self::Msg>,
     ) -> bool {
+        match msg {
+            Message::LoginSuccessful => self.page = Page::Main,
+        }
         true
     }
 }
@@ -31,46 +42,80 @@ impl Model for AppModel {
 impl Widgets<AppModel, ()> for AppWidgets {
     view! {
         main_window = ApplicationWindow {
-            set_content: main_box = Some(&Box) {
-                set_orientation: Orientation::Vertical,
-                append = &HeaderBar {
-                    set_title_widget = Some(&Label) {
-                        set_label: "GTK4 QQ"
-                    },
-                    pack_end: &Button::from_icon_name("go-next"),
-                    pack_end: &Button::from_icon_name("dialog-information"),
-                },
-                append = &Box {
-                    set_halign: Align::Center,
-                    set_valign: Align::Center,
-                    set_vexpand: true,
+            set_content: stack = Some(&Stack) {
+                set_transition_type: StackTransitionType::SlideLeft,
+                add_child: login_panel = &Box {
                     set_orientation: Orientation::Vertical,
+                    append = &HeaderBar {
+                        set_title_widget = Some(&Label) {
+                            set_label: "Login"
+                        },
+                        pack_end = &Button {
+                            set_icon_name: "go-next",
+                            connect_clicked(sender) => move |_| {
+                                send!(sender, Message::LoginSuccessful);
+                            },
+                        },
+                        pack_end: &Button::from_icon_name("dialog-information"),
+                    },
                     append = &Box {
-                        append = &Grid {
-                            set_row_spacing: 12,
-                            set_column_spacing: 12,
-                            attach(0, 0, 1, 1) = &Label {
-                                set_label: "Account"
-                            },
-                            attach(1, 0, 1, 1) = &Entry {
-                                set_placeholder_text: Some("请输入您的QQ号码")
-                            },
-                            attach(0, 1, 1, 1) = &Label {
-                                set_label: "Password"
-                            },
-                            attach(1, 1, 1, 1) = &Entry {
-                                set_placeholder_text: Some("请输入您的QQ密码")
+                        set_halign: Align::Center,
+                        set_valign: Align::Center,
+                        set_vexpand: true,
+                        set_orientation: Orientation::Vertical,
+                        append = &Box {
+                            append = &Grid {
+                                set_row_spacing: 12,
+                                set_column_spacing: 12,
+                                attach(0, 0, 1, 1) = &Label {
+                                    set_label: "Account"
+                                },
+                                attach(1, 0, 1, 1) = &Entry {
+                                    set_placeholder_text: Some("请输入您的QQ号码")
+                                },
+                                attach(0, 1, 1, 1) = &Label {
+                                    set_label: "Password"
+                                },
+                                attach(1, 1, 1, 1) = &Entry {
+                                    set_placeholder_text: Some("请输入您的QQ密码")
+                                }
                             }
                         }
                     }
+                },
+                add_child: main_panel = &Box {
+                    set_orientation: Orientation::Vertical,
+                    append = &HeaderBar {
+                        set_title_widget = Some(&Label) {
+                            set_label: "GTK4 QQ"
+                        },
+                    },
+                    append = &Box {
+                        set_halign: Align::Center,
+                        set_valign: Align::Center,
+                        set_vexpand: true,
+                        set_orientation: Orientation::Vertical,
+                        append = &Box {
+                            append = &Label {
+                                set_label: "Hello, World!"
+                            },
+                        }
+                    }
                 }
+
             }
+        }
+    }
+    fn pre_view() {
+        match model.page {
+            Page::Login => self.stack.set_visible_child(&self.login_panel),
+            Page::Main => self.stack.set_visible_child(&self.main_panel),
         }
     }
 }
 
 fn main() {
-    let model = AppModel::default();
+    let model = AppModel { page: Page::Login };
     let app = RelmApp::new(model);
     app.run()
 }
