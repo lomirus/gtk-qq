b597e79074739daf4868551bcf9131b114553ab9
lomirus <lomirus@qq.com>
2022-05-09 14:41:17 +0800

    feat(ui/main): add libadwaita leaflet

--- src/pages/main.rs
+++ src/pages/main.rs
@@ -1,9 +1,8 @@
 use relm4::{adw, gtk, ComponentUpdate, Model, Sender, Widgets};
 
+use adw::prelude::*;
 use adw::HeaderBar;
 use gtk::{Align, Box, Label, Orientation};
-
-use adw::prelude::*;
 
 use crate::{AppModel, Message};
 
@@ -33,26 +32,45 @@ impl ComponentUpdate<AppModel> for MainPageModel {
 #[relm4::widget(pub)]
 impl Widgets<MainPageModel, AppModel> for MainPageWidgets {
     view! {
-        &Box {
-            set_hexpand: true,
-            set_vexpand: true,
-            set_orientation: Orientation::Vertical,
-            append = &HeaderBar {
-                set_title_widget = Some(&Label) {
-                    set_label: "GTK4 QQ"
-                },
-            },
-            append = &Box {
-                set_halign: Align::Center,
-                set_valign: Align::Center,
+        &adw::Leaflet {
+            set_can_navigate_forward: true,
+            append: sidebar = &Box {
                 set_vexpand: true,
+                set_width_request: 360,
                 set_orientation: Orientation::Vertical,
+                append = &HeaderBar {
+                    set_title_widget = Some(&Label) {
+                        set_label: "Contact"
+                    },
+                },
                 append = &Box {
+                    set_vexpand: true,
+                    set_valign: Align::Center,
+                    set_halign: Align::Center,
                     append = &Label {
-                        set_label: "Hello, World!"
+                        set_label: "Sidebar"
                     },
                 }
-            }
+            },
+            append: &gtk::Separator::default(),
+            append: chatroom = &Box {
+                set_vexpand: true,
+                set_hexpand: true,
+                set_orientation: Orientation::Vertical,
+                append = &HeaderBar {
+                    set_title_widget = Some(&Label) {
+                        set_label: "Chatroom"
+                    },
+                },
+                append = &Box {
+                    set_vexpand: true,
+                    set_valign: Align::Center,
+                    set_halign: Align::Center,
+                    append = &Label {
+                        set_label: "Chatroom"
+                    },
+                }
+            },
         }
     }
 }
