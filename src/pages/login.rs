e8545cd59fcf87441e540fc31a6570286fb6ecc9
lomirus <lomirus@qq.com>
2022-05-09 11:20:18 +0800

    feat(ui/login): push toast when input value is empty

--- src/pages/login.rs
+++ src/pages/login.rs
@@ -1,9 +1,10 @@
+use std::collections::VecDeque;
+
 use relm4::{adw, gtk, send, ComponentUpdate, Model, Sender, Widgets};
 
-use adw::{ActionRow, Avatar, HeaderBar, PreferencesGroup};
-use gtk::{Align, Box, Button, Entry, Label, Orientation};
-
 use adw::prelude::*;
+use adw::{ActionRow, Avatar, HeaderBar, PreferencesGroup, Toast, ToastOverlay};
+use gtk::{Align, Box, Button, Entry, Label, Orientation};
 
 use crate::{AppModel, Message};
 
@@ -11,6 +12,7 @@ use crate::{AppModel, Message};
 pub struct LoginPageModel {
     account: String,
     password: String,
+    toast_stack: VecDeque<String>,
 }
 
 impl Model for LoginPageModel {
@@ -24,6 +26,8 @@ pub enum LoginPageMsg {
     LoginSuccessful,
     AccountChange(String),
     PasswordChange(String),
+    PushToast(String),
+    ShiftToast
 }
 
 impl ComponentUpdate<AppModel> for LoginPageModel {
@@ -42,11 +46,23 @@ impl ComponentUpdate<AppModel> for LoginPageModel {
         match msg {
             LoginStart => {
                 println!("{:?}", self);
+                if self.account == "" {
+                    send!(sender, PushToast("Account cannot be empty".to_string()));
+                    return;
+                }
+                if self.password == "" {
+                    send!(sender, PushToast("Password cannot be empty".to_string()));
+                    return;
+                }
                 send!(sender, LoginPageMsg::LoginSuccessful)
             }
             LoginSuccessful => send!(parent_sender, Message::LoginSuccessful),
             AccountChange(new_account) => self.account = new_account,
             PasswordChange(new_password) => self.password = new_password,
+            PushToast(message) => self.toast_stack.push_back(message),
+            ShiftToast => {
+                self.toast_stack.pop_front().expect("failed to pop from toast stack");
+            }
         }
     }
 }
@@ -70,38 +86,48 @@ impl Widgets<LoginPageModel, AppModel> for LoginPageWidgets {
                 },
                 pack_end: &Button::from_icon_name("dialog-information"),
             },
-            append = &Box {
-                set_halign: Align::Center,
-                set_valign: Align::Center,
-                set_vexpand: true,
-                set_spacing: 32,
-                append = &Avatar {
-                    set_text: Some("ADW"),
-                    set_size: 72,
-                },
-                append = &PreferencesGroup {
-                    add = &ActionRow {
-                        set_title: "Account",
-                        add_suffix = &Entry {
-                            set_valign: Align::Center,
-                            set_placeholder_text: Some("请输入您的QQ号码"),
-                            connect_activate(sender) => move |e| {
-                                sender.send(LoginPageMsg::AccountChange(e.buffer().text())).unwrap();
-                            }
+            append: toast_overlay = &ToastOverlay {
+                set_child = Some(&Box) {
+                    set_halign: Align::Center,
+                    set_valign: Align::Center,
+                    set_vexpand: true,
+                    set_spacing: 32,
+                    append = &Avatar {
+                        set_text: Some("ADW"),
+                        set_size: 72,
+                    },
+                    append = &PreferencesGroup {
+                        add = &ActionRow {
+                            set_title: "Account",
+                            add_suffix = &Entry {
+                                set_valign: Align::Center,
+                                set_placeholder_text: Some("请输入您的QQ号码"),
+                                connect_changed(sender) => move |e| {
+                                    sender.send(LoginPageMsg::AccountChange(e.buffer().text())).unwrap();
+                                }
+                            },
+                        },
+                        add = &ActionRow {
+                            set_title: "Password",
+                            add_suffix = &Entry {
+                                set_valign: Align::Center,
+                                set_placeholder_text: Some("请输入您的QQ密码"),
+                                connect_changed(sender) => move |e| {
+                                    sender.send(LoginPageMsg::PasswordChange(e.buffer().text())).unwrap();
+                                }
+                            },
                         },
                     },
-                    add = &ActionRow {
-                        set_title: "Password",
-                        add_suffix = &Entry {
-                            set_valign: Align::Center,
-                            set_placeholder_text: Some("请输入您的QQ密码"),
-                            connect_activate(sender) => move |e| {
-                                sender.send(LoginPageMsg::PasswordChange(e.buffer().text())).unwrap();
-                            }
-                        },
-                    },
                 },
-            }
+            },
+        }
+    }
+
+    fn pre_view() {
+        if !model.toast_stack.is_empty() {
+            let toast_message = model.toast_stack[0].as_str();
+            send!(sender, LoginPageMsg::ShiftToast);
+            self.toast_overlay.add_toast(&Toast::new(toast_message));
         }
     }
 }
