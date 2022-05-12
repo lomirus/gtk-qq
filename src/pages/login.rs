8d07436b5a178a2c6ab10cfaa8502d66d45eea33
lomirus <lomirus@qq.com>
2022-05-12 20:25:48 +0800

    feat(ui/login): add menu popover

--- src/pages/login.rs
+++ src/pages/login.rs
@@ -1,10 +1,11 @@
 use std::collections::VecDeque;
 
+use relm4::actions::{RelmActionGroup, RelmAction};
 use relm4::{adw, gtk, send, ComponentUpdate, Model, Sender, Widgets};
 
 use adw::prelude::*;
 use adw::{ActionRow, Avatar, HeaderBar, PreferencesGroup, Toast, ToastOverlay};
-use gtk::{Align, Box, Button, Entry, Label, Orientation};
+use gtk::{Align, Box, Button, Entry, Label, MenuButton, Orientation};
 
 use crate::{AppModel, Message};
 
@@ -27,7 +28,7 @@ pub enum LoginPageMsg {
     AccountChange(String),
     PasswordChange(String),
     PushToast(String),
-    ShiftToast
+    ShiftToast,
 }
 
 impl ComponentUpdate<AppModel> for LoginPageModel {
@@ -61,7 +62,9 @@ impl ComponentUpdate<AppModel> for LoginPageModel {
             PasswordChange(new_password) => self.password = new_password,
             PushToast(message) => self.toast_stack.push_back(message),
             ShiftToast => {
-                self.toast_stack.pop_front().expect("failed to pop from toast stack");
+                self.toast_stack
+                    .pop_front()
+                    .expect("failed to pop from toast stack");
             }
         }
     }
@@ -70,7 +73,7 @@ impl ComponentUpdate<AppModel> for LoginPageModel {
 #[relm4::widget(pub)]
 impl Widgets<LoginPageModel, AppModel> for LoginPageWidgets {
     view! {
-        &Box {
+        login_page = Box {
             set_hexpand: true,
             set_vexpand: true,
             set_orientation: Orientation::Vertical,
@@ -84,7 +87,10 @@ impl Widgets<LoginPageModel, AppModel> for LoginPageWidgets {
                         send!(sender, LoginPageMsg::LoginStart);
                     },
                 },
-                pack_end: &Button::from_icon_name("dialog-information"),
+                pack_end = &MenuButton {
+                    set_icon_name: "menu-symbolic",
+                    set_menu_model: Some(&main_menu),
+                }
             },
             append: toast_overlay = &ToastOverlay {
                 set_child = Some(&Box) {
@@ -121,6 +127,32 @@ impl Widgets<LoginPageModel, AppModel> for LoginPageWidgets {
                 },
             },
         }
+    }
+
+    menu! {
+        main_menu: {
+            "Keyboard Shortcuts" => ShortcutsAction,
+            "About Gtk QQ" => AboutAction
+        }
+    }
+
+    fn post_init() {
+        relm4::new_action_group!(WindowActionGroup, "menu");
+        relm4::new_stateless_action!(ShortcutsAction, WindowActionGroup, "shortcuts");
+        relm4::new_stateless_action!(AboutAction, WindowActionGroup, "about");
+        
+        let shortcuts_action: RelmAction<ShortcutsAction> = RelmAction::new_stateless(move |_| {
+            println!("Keyboard Shortcuts");
+        });
+        let about_action: RelmAction<AboutAction> = RelmAction::new_stateless(move |_| {
+            println!("About Gtk QQ");
+        });
+        let group: RelmActionGroup<WindowActionGroup> = RelmActionGroup::new();
+        group.add_action(shortcuts_action);
+        group.add_action(about_action);
+
+        let actions = group.into_action_group();
+        login_page.insert_action_group("menu", Some(&actions));
     }
 
     fn pre_view() {
