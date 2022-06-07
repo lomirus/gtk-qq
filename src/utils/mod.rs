19562e91c12fb5e3e8995155be87e784ed907d7b
Frozen String <964413011@qq.com>
2022-06-07 23:49:05 +0800

    💚 fix clippy

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -14,10 +14,9 @@ static BASE_DIR_PATH: OnceCell<PathBuf> = OnceCell::new();
 pub(crate) fn init_gtk_qq_dir() -> Result<&'static PathBuf, io::Error> {
     BASE_DIR_PATH.get_or_try_init(|| {
         let path = dirs::home_dir()
-            .ok_or(io::Error::new(
-                io::ErrorKind::NotFound,
-                "user home directory not found",
-            ))?
+            .ok_or_else(|| {
+                io::Error::new(io::ErrorKind::NotFound, "user home directory not found")
+            })?
             .join(BASE_DIR);
 
         std::fs::create_dir(&path)?;
