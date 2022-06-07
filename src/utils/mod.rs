75050c86853b74437f0f661e7de6d8a57ed68798
Frozen String <964413011@qq.com>
2022-06-07 22:10:16 +0800

    🚧 添加avatars辅助相关代码

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,1 +1,19 @@
+use std::{
+    io::{self},
+    path::PathBuf,
+};
 
+pub mod avatar;
+
+static BASE_DIR: &str = ".gtk-qq";
+
+pub(crate) fn init_gtk_qq_dir() -> Result<PathBuf, io::Error> {
+    let mut path = dirs::home_dir().ok_or(io::Error::new(
+        io::ErrorKind::NotFound,
+        "user home directory not found",
+    ))?;
+    std::fs::create_dir(&path)?;
+    path.push(BASE_DIR);
+
+    Ok(path)
+}
