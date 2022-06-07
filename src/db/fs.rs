75c6bf247c2cf9c16542131d174b12e1ca55eb7b
lomirus <lomirus@qq.com>
2022-06-07 23:02:28 +0800

    refactor(db): replace sync fs api with tokio api

--- src/db/fs.rs
+++ src/db/fs.rs
@@ -1,21 +1,20 @@
-use std::fs::{create_dir_all, File};
-use std::io::Write;
 use std::path::PathBuf;
+
+use tokio::fs::{create_dir_all, write};
 
 pub async fn download_user_avatar_file(user_id: i64) {
     let mut path = dirs::home_dir().unwrap();
     path.push(".gtk-qq");
     path.push("avatars");
     path.push("users");
-    create_dir_all(path.clone()).unwrap();
+    create_dir_all(path.clone()).await.unwrap();
     path.push(format!("{}.png", user_id));
-    let mut file = File::create(path).unwrap();
 
     let url = format!("http://q2.qlogo.cn/headimg_dl?dst_uin={}&spec=160", user_id);
     println!("Downloading {}", url);
     let body = reqwest::get(url).await.unwrap().bytes().await.unwrap();
 
-    file.write_all(&body).unwrap();
+    write(path, body).await.unwrap();
 }
 
 pub fn get_user_avatar_path(user_id: i64) -> PathBuf {
@@ -33,15 +32,14 @@ pub async fn download_group_avatar_file(group_id: i64) {
     path.push(".gtk-qq");
     path.push("avatars");
     path.push("groups");
-    create_dir_all(path.clone()).unwrap();
+    create_dir_all(path.clone()).await.unwrap();
     path.push(format!("{}.png", group_id));
-    let mut file = File::create(path).unwrap();
 
     let url = format!("https://p.qlogo.cn/gh/{}/{}/0", group_id, group_id);
     println!("Downloading {}", url);
     let body = reqwest::get(url).await.unwrap().bytes().await.unwrap();
 
-    file.write_all(&body).unwrap();
+    write(path, body).await.unwrap();
 }
 
 pub fn get_group_avatar_path(group_id: i64) -> PathBuf {
