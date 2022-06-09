49e4ead79a1ea576c96f572a749d03b5d5c92b74
Frozen String <964413011@qq.com>
2022-06-10 01:33:12 +0800

    🎨 cargo fmt 格式化代码

--- src/utils/widgets/linker_copier/mod.rs
+++ src/utils/widgets/linker_copier/mod.rs
@@ -1,8 +1,7 @@
-mod widgets;
 mod builder;
 mod component;
+mod widgets;
 
-
+pub use builder::{LinkCopierState, LinkerCopierCfg};
 pub use component::LinkCopierModel;
 pub use widgets::LinkCopierWidgets;
-pub use builder::{LinkCopierState,LinkerCopierCfg};
