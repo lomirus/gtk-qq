ae51cc233ab8d4abdd0b52635a97495b5a58b38e
Frozen String <964413011@qq.com>
2022-06-09 18:02:20 +0800

    ♻️ 没有想法的重构（）

--- src/utils/widgets/mod.rs
+++ src/utils/widgets/mod.rs
@@ -1,1 +1,15 @@
+use relm4::gtk::{prelude::IsA, Widget};
+
 pub mod linker_copier;
+
+/// 摆烂级
+pub trait CustomWidget<Message> {
+    type Widget: IsA<Widget>;
+
+    fn into_widget(self) -> Self::Widget;
+    fn into_widget_ref(&self) -> &Self::Widget;
+}
+
+pub trait InternalBuilder<W> {
+    fn get_internal(&mut self)->&mut W;   
+}
