0bfcc7f06964fdac2d73fcc0b5872ade494b4788
Frozen String <964413011@qq.com>
2022-06-18 21:53:11 +0800

    ♻️ (db-res) impl `get_and_create_path` manual

--- libs/resource-loader/src/ops/database.rs
+++ libs/resource-loader/src/ops/database.rs
@@ -1,3 +1,5 @@
+use std::{io, path::Path, fs::create_dir_all};
+
 use crate::static_data::load_cfg;
 
 use super::GetPath;
@@ -5,9 +7,19 @@ use super::GetPath;
 pub struct SqlDataBase;
 
 impl GetPath for SqlDataBase {
-    fn get_path() -> &'static std::path::Path {
+    fn get_path() -> &'static Path {
         let cfg = load_cfg();
 
         cfg.database.sql_data
+    }
+
+    fn get_and_create_path() -> io::Result<&'static Path> {
+        let path = <Self as GetPath>::get_path();
+
+        if let Some(path) = path.parent() {
+            create_dir_all(path)?;
+        }
+
+        Ok(path)
     }
 }
