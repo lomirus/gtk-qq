7bc8010894196f896acfdc105e58438f7090b5ee
Frozen String <964413011@qq.com>
2022-07-04 19:28:48 +0800

    🎨 cargo fmt

--- libs/widgets/src/qrcode_login/mod.rs
+++ libs/widgets/src/qrcode_login/mod.rs
@@ -4,7 +4,7 @@ mod payloads;
 mod widgets;
 
 pub use component::QrCodeLoginModel;
-pub use payloads::{Input,Output,PayLoad};
+pub use payloads::{Input, Output, PayLoad};
 pub use widgets::QrCodeLoginWidgets;
 
 pub type QrCodeLogin = relm4::Controller<QrCodeLoginModel>;
