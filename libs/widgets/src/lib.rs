3e3b3845a4b3722cdf77b1f2f664731471111258
Frozen String <964413011@qq.com>
2022-07-04 16:16:20 +0800

    ✨ (qr_code_login) 添加二维码登录组件

--- libs/widgets/src/lib.rs
+++ libs/widgets/src/lib.rs
@@ -1,1 +1,2 @@
+pub mod qrcode_login;
 pub mod link_copier;
---
+++ libs/widgets/src/qrcode_login/payloads.rs
@@ -0,0 +1,23 @@
+use std::{path::Path, sync::Arc};
+
+use bytes::Bytes;
+use relm4::adw::Window;
+use ricq::{Client, LoginResponse, RQError};
+
+pub enum Input {
+    UpdateQrCode,
+    Updated,
+    FollowLogin(LoginResponse),
+    Error(RQError),
+}
+
+pub enum Output {
+    LoginGoAhead(LoginResponse),
+    Error(RQError),
+}
+
+pub struct PayLoad {
+    pub client: Arc<Client>,
+    pub windows: Window,
+    pub temp_img_path: &'static Path,
+}
