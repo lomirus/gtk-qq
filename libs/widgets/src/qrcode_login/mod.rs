4f6d5e68fd0e461e7a2d348ff79ca64fb9b6ca41
Frozen String <964413011@qq.com>
2022-07-04 19:28:20 +0800

    🚧 调整新的组件导出

--- libs/widgets/src/qrcode_login/mod.rs
+++ libs/widgets/src/qrcode_login/mod.rs
@@ -2,3 +2,9 @@ mod background;
 mod component;
 mod payloads;
 mod widgets;
+
+pub use component::QrCodeLoginModel;
+pub use payloads::{Input,Output,PayLoad};
+pub use widgets::QrCodeLoginWidgets;
+
+pub type QrCodeLogin = relm4::Controller<QrCodeLoginModel>;
