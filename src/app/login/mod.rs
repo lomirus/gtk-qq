2b3f0f88b3f9eb41abdfc5fd929ed4c1015a9728
Frozen String <964413011@qq.com>
2022-07-13 10:27:27 +0800

    ♻️ (pwd-login) 替换原有的登录界面

--- src/app/login/mod.rs
+++ src/app/login/mod.rs
@@ -5,24 +5,25 @@ mod service;
 use std::sync::Arc;
 
 use once_cell::sync::OnceCell;
+use relm4::gtk::gdk::Paintable;
 use relm4::{
     adw, gtk, Component, ComponentController, ComponentParts, ComponentSender, SimpleComponent,
 };
 
 use adw::prelude::*;
-use adw::{ActionRow, Avatar, HeaderBar, PreferencesGroup, Toast, ToastOverlay, Window};
-use gtk::gdk::Paintable;
+use adw::{HeaderBar, Toast, ToastOverlay, Window};
+
 use gtk::gdk_pixbuf::Pixbuf;
-use gtk::{Align, Box, Button, Entry, EntryBuffer, Label, MenuButton, Orientation, Picture};
+use gtk::{Box, Label, MenuButton, Orientation, Picture};
 
 use ricq::Client;
 use tokio::task;
+use widgets::pwd_login::{self, Input, PasswordLogin, PasswordLoginModel, Payload};
 
 use crate::actions::{AboutAction, ShortcutsAction};
 use crate::app::AppMessage;
 use crate::db::fs::{download_user_avatar_file, get_user_avatar_path};
 use crate::global::WINDOW;
-use crate::utils::avatar::loader::{AvatarLoader, User};
 
 use self::service::{get_login_info, login};
 
@@ -35,18 +36,17 @@ pub static LOGIN_SENDER: OnceCell<ComponentSender<LoginPageModel>> = OnceCell::n
 
 #[derive(Debug)]
 pub struct LoginPageModel {
-    account: String,
-    password: String,
-    is_login_button_enabled: bool,
+    // account: String,
+    // password: String,
+    // is_login_button_enabled: bool,
+    pwd_login: PasswordLogin,
     toast: Option<String>,
 }
 
 pub enum LoginPageMsg {
-    LoginStart,
+    Login(i64, String),
     LoginSuccessful,
     LoginFailed(String),
-    AccountChange(String),
-    PasswordChange(String),
     NeedCaptcha(String, Arc<Client>, UserId, Password),
     DeviceLock(VerifyUrl, SmsPhone),
     ConfirmVerification,
@@ -68,33 +68,22 @@ impl SimpleComponent for LoginPageModel {
         if LOGIN_SENDER.set(sender.clone()).is_err() {
             panic!("failed to initialize login sender");
         }
+        let (account, password) = get_login_info();
+        let avatar = load_avatar(account.parse().ok(), true);
+
+        let pwd_login = PasswordLoginModel::builder()
+            .launch(Payload {
+                account: account.parse().ok(),
+                password: password.into(),
+                avatar: avatar,
+            })
+            .forward(sender.input_sender(), |out| match out {
+                pwd_login::Output::Login { account, pwd } => LoginPageMsg::Login(account, pwd),
+            });
 
         let widgets = view_output!();
-
-        let (account, password) = get_login_info();
-        let account_buffer = EntryBuffer::new(Some(&account));
-        let password_buffer = EntryBuffer::new(Some(&password));
-        widgets.account_entry.set_buffer(&account_buffer);
-        widgets.password_entry.set_buffer(&password_buffer);
-
-        if let Ok(account) = account.parse::<i64>() {
-            let path = get_user_avatar_path(account);
-            if path.exists() {
-                if let Ok(pixbuf) = Pixbuf::from_file_at_size(path, 96, 96) {
-                    let image = Picture::for_pixbuf(&pixbuf);
-                    if let Some(paintable) = image.paintable() {
-                        widgets.avatar.set_custom_image(Some(&paintable));
-                    }
-                }
-            } else {
-                task::spawn(download_user_avatar_file(account));
-            }
-        }
-
         let model = LoginPageModel {
-            account,
-            password,
-            is_login_button_enabled: true,
+            pwd_login,
             toast: None,
         };
 
@@ -104,35 +93,15 @@ impl SimpleComponent for LoginPageModel {
     fn update(&mut self, msg: LoginPageMsg, sender: &ComponentSender<Self>) {
         use LoginPageMsg::*;
         match msg {
-            LoginStart => {
-                // Get the account
-                let account: i64 = match self.account.parse::<i64>() {
-                    Ok(account) => account,
-                    Err(_) => {
-                        self.toast = Some("Account is invalid".to_string());
-                        return;
-                    }
-                };
-                // Get the password
-                let password = if self.password.is_empty() {
-                    self.toast = Some("Password cannot be empty".to_string());
-                    return;
-                } else {
-                    self.password.to_string()
-                };
-
-                self.is_login_button_enabled = false;
-                task::spawn(login(account, password));
+            Login(uin, pwd) => {
+                task::spawn(login(uin, pwd));
             }
             LoginSuccessful => {
                 sender.output(AppMessage::LoginSuccessful);
             }
             LoginFailed(msg) => {
                 self.toast = Some(msg);
-                self.is_login_button_enabled = true;
             }
-            AccountChange(new_account) => self.account = new_account,
-            PasswordChange(new_password) => self.password = new_password,
             NeedCaptcha(verify_url, client, account, password) => {
                 sender.input(LoginPageMsg::LoginFailed(
                     "Need Captcha. See more in the pop-up window.".to_string(),
@@ -180,7 +149,7 @@ impl SimpleComponent for LoginPageModel {
                 window.present()
             }
             // TODO: proc follow operate
-            ConfirmVerification => sender.input(LoginStart),
+            ConfirmVerification => self.pwd_login.emit(Input::Login),
         }
     }
 
@@ -201,12 +170,6 @@ impl SimpleComponent for LoginPageModel {
                 set_title_widget = Some(&Label) {
                     set_label: "Login"
                 },
-                pack_end: go_next_button = &Button {
-                    set_icon_name: "go-next",
-                    connect_clicked[sender] => move |_| {
-                        sender.input(LoginPageMsg::LoginStart);
-                    },
-                },
                 pack_end = &MenuButton {
                     set_icon_name: "menu-symbolic",
                     set_menu_model: Some(&main_menu),
@@ -214,41 +177,7 @@ impl SimpleComponent for LoginPageModel {
             },
             #[name = "toast_overlay"]
             ToastOverlay {
-                set_child = Some(&Box) {
-                    set_halign: Align::Center,
-                    set_valign: Align::Center,
-                    set_vexpand: true,
-                    set_spacing: 32,
-                    #[name = "avatar"]
-                    Avatar {
-                        set_size: 96,
-                    },
-                    PreferencesGroup {
-                        add = &ActionRow {
-                            set_title: "Account",
-                            set_focusable: false,
-                            add_suffix: account_entry = &Entry {
-                                set_valign: Align::Center,
-                                set_placeholder_text: Some("Please input your QQ account "),
-                                connect_changed[sender] => move |e| {
-                                    sender.input(LoginPageMsg::AccountChange(e.buffer().text()));
-                                }
-                            },
-                        },
-                        add = &ActionRow {
-                            set_title: "Password",
-                            set_focusable: false,
-                            add_suffix: password_entry = &Entry {
-                                set_valign: Align::Center,
-                                set_placeholder_text: Some("Please input your QQ password"),
-                                set_visibility: false,
-                                connect_changed[sender] => move |e| {
-                                    sender.input(LoginPageMsg::PasswordChange(e.buffer().text()));
-                                }
-                            },
-                        },
-                    },
-                },
+                set_child : Some(pwd_login.widget()),
             }
         }
     }
@@ -257,20 +186,23 @@ impl SimpleComponent for LoginPageModel {
         if let Some(content) = &self.toast {
             widgets.toast_overlay.add_toast(&Toast::new(content));
         }
-        widgets
-            .go_next_button
-            .set_sensitive(self.is_login_button_enabled);
-
-        let paint = self
-            .account
-            .parse::<i64>()
-            .ok()
-            .and_then(|id| User::get_avatar_as_pixbuf(id, 96, 96).ok())
-            .map(|pix_buf| Picture::for_pixbuf(&pix_buf))
-            .and_then(|pic| pic.paintable());
-
-        widgets
-            .avatar
-            .set_custom_image(Into::<Option<&'_ Paintable>>::into(&paint));
     }
+}
+
+fn load_avatar(account: Option<i64>, auto_download: bool) -> Option<Paintable> {
+    account
+        .map(|uin| (uin, get_user_avatar_path(uin)))
+        .and_then(|(uin, path)| {
+            if path.exists() {
+                Some(path)
+            } else {
+                if auto_download {
+                    task::spawn(download_user_avatar_file(uin));
+                }
+                None
+            }
+        })
+        .and_then(|path| Pixbuf::from_file_at_size(path, 96, 96).ok())
+        .map(|pix| Picture::for_pixbuf(&pix))
+        .and_then(|pic| pic.paintable())
 }
